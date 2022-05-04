410e8016519b76ffa4a3623411f4fbe688418ab5
lelongnhat <96115946+lelongnhat@users.noreply.github.com>
2022-05-04 22:00:33 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa:3333",
+  text1: "He luu ebe của anh :>!",
+  text2: "Anh có điều này muốn hỏi em nhớ phải trả lời thật lòng nhaaa:3333",
   text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text4: "Nếu em ko trả lời mà thoát ra tức là muốn làm vợ anh rùi đó nha :v",
+  text5: "Anh mơ à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll:))))",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "lí do em thích tớ đi :vvvv",
+  text8: "Gửi cho anh <3",
+  text9: "Vì anh đẹp try và cute vlllll:))))",
+  text10: "Anh  biết mà ^^ Yêu em 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "mai gặp dc khum :>",
   text12: "Okii lunn <3",
 };
 
